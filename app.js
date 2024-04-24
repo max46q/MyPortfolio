75b964cb163ba76d9c3020b837081ceb0bf196eb
max46q <2000maksym2000@gmail.com>
2024-04-24 21:27:49 +0300

    up

--- app.js
+++ app.js
@@ -4,20 +4,30 @@ const ejs = require("ejs");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static("public")); // Add this line
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
   res.render("home.ejs"); // відкриває html файл
 });
-// app.post('/add', (req, res) => {
-//   const name = req.body.name
-//   const email = req.body.email
 
-//   res.render('result', { name, email })
-// })
 app.get("/base", function (req, res) {
   res.render("otherFile");
 });
-app.listen(3000);
+
+app.post("/add", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+
+  res.render("result", { name, email });
+});
+app.post("/aa", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  console.log(username, password);
+  res.redirect("/")
+});
+
+app.listen(3000); //port
 console.log("server started");
