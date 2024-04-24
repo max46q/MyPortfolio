bfb5500899611b3a577a8276993cb832eb77e3f6
max46q <2000maksym2000@gmail.com>
2024-04-24 21:48:00 +0300

    up cheack admin and password

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+require("dotenv").config();
+app.engine("ejs", require("ejs").renderFile);
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
-app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  res.render("home.ejs"); // відкриває html файл
+  let error = "";
+  res.render("Home", { error }); // відкриває html файл
 });
 
 app.get("/base", function (req, res) {
@@ -18,15 +21,20 @@ app.get("/base", function (req, res) {
 app.post("/add", (req, res) => {
   const name = req.body.name;
   const email = req.body.email;
-
+  
   res.render("result", { name, email });
 });
 app.post("/aa", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-
-  console.log(username, password);
-  res.redirect("/")
+  if (username === process.env.USERNAME1 && password === process.env.PASSWORD) {
+    console.log("hello");
+    res.redirect("/");
+  } else {
+    console.log("error");
+    let error = "геть не той пароль";
+    res.render("Home", { error });
+  }
 });
 
 app.listen(3000); //port
